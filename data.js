19e4e86086a9039515c8c633c3ddb878dea77311
Oyaro Jared <gerraldoyaro@gmail.com>
2020-12-28 13:47:29 +0300

    edited again

---
+++ data.js
@@ -0,0 +1,135 @@
+let data = [
+  {
+    question: 'When did Kenya gain independence?',
+    answers: ['1992', '1990', '1963', '1920'],
+    get corrAns () {
+      return this.answers[2];
+    }
+  },
+
+  {
+    question: 'Which one of the following is not a branch of Computer Science?',
+    answers: ['Data Collection', 'Data Science', 'Artificial Intelligence', 'Software Engineering'],
+    get corrAns () {
+      return this.answers[0];
+    }
+  },
+
+  {
+    question: 'Which one of the following is not a programming language?',
+    answers: ['Python', 'HTML', 'C++', 'C#'],
+    get corrAns () {
+      return this.answers[1];
+    }
+  },
+
+  {
+    question: '(a = 53) === (a === "53")',
+    answers: ['True', 'False', 'Undefined', 'Null'],
+    get corrAns () {
+      return this.answers[1];
+    }
+  },
+
+  {
+    question: 'Pick the odd one out?',
+    answers: ['Raila', 'Uhuru', 'Obama', 'Trump'],
+    get corrAns () {
+      return this.answers[0];
+    }
+  },
+
+  {
+    question: 'What is the capital city of Kenya?',
+    answers: ['Kisumu', 'Nairobi', 'Mombasa', 'Dodoma'],
+    get corrAns () {
+      return this.answers[1];
+    }
+  },
+
+  {
+    question: 'Pick the odd one out',
+    answers: ['Python', 'CSS', 'C++', 'C#'],
+    get corrAns () {
+      return this.answers[1];
+    }
+  },
+
+  {
+    question: 'CSS stands for?',
+    answers: ['Casscading Styles Sheets', 'Common Styles Sheets', 'Casscading Style Scripts', 'Common Styles Script'],
+    get corrAns () {
+      return this.answers[0];
+    }
+  },
+
+  {
+    question: 'Start a multiline JavaScript comment.',
+    answers: ['/*', '*/', '/* */', '//'],
+    get corrAns () {
+      return this.answers[0];
+    }
+  },
+
+  {
+    question: 'Which one of the following  statement is false about JavaScript?',
+    answers: ['Arrays are objects', 'functions are variables', 'constants cannot be reassigned', 'all of the above'],
+    get corrAns () {
+      return this.answers[3];
+    }
+  },
+
+
+  {
+    question: 'What is you favourite color?',
+    answers: ['blue', 'black', 'white', 'red'],
+    get corrAns () {
+      return this.answers[0];
+    }
+  },
+
+];
+
+/*
+document.getElementById('examCard').insertAdjacentHTML('afterbegin', `<div class="answers-holder">
+<h3 id="quzz">Click Next to start the test</h3>
+ <div class="answers">
+      <div class="ans1">
+        <input type="radio" name="ans" id="firstAns">
+        <label for="firstAns" id="a"></label>
+      </div>
+      <div class="ans2">
+        <input type="radio" name="ans" id="secondAns">
+        <label for="secondAns" id="b"></label>
+      </div>
+      <div class="ans3">
+        <input type="radio" name="ans" id="thirdAns">
+        <label for="thirdAns" id="c"></label>
+      </div>
+      <div class="ans4">
+        <input type="radio" name="ans" id="fouthAns">
+        <label for="fouthAns" id="d"></label>
+      </div>
+    
+
+   <div class="navigation">
+     <button class="next-btn btn1 btn">Next</button>
+   </div>
+   </div>`) */
+
+
+
+   function insertResultDiv() {
+     return `<div class="score-output card">
+     <h3>RESULT</h3>
+      <div class="total-quez">
+        <input type="number" placeholder="Total Questions: " id ="totQue" readonly>
+      </div>
+      <div class="correct-score">
+        <input type="number" placeholder="Score: "  id = "corrScore" readonly>
+      </div>
+      <div class="perc">
+        <input type="number" placeholder="Percentage: "  id = "per" readonly>
+      </div> 
+      </div>`;
+   }
--- script.js
+++ script.js
@@ -1,170 +1,121 @@
 
- 
+document.addEventListener('DOMContentLoaded', onNextBtnPress);
 
-document.addEventListener('DOMContentLoaded', () => {
-  var index = 0;
-  Array.from(document.querySelectorAll('.btn1')).forEach(btn => {
-    btn.addEventListener('click', () => {
-         var bars = Array.from(document.getElementsByClassName('edit'));
-         if (index < 10) {
-          bars[index].classList.add('bg-color');
-          index++;
-          reset()
-          startTimer();
-          loadQuzz(index)
+
+let init = {
+  score : 0,
+  radios : Array.from(document.getElementsByName('ans')),
+  index : 0
+}
+
+
+function onNextBtnPress() {
+  const NEXT_BTN = document.querySelector('.btn1'),
+        BARS = Array.from(document.getElementsByClassName('edit'));
+
+        function loadFunctionalities () {
+            BARS[init.index].classList.add('bg-color');
+            resetTimer();
+            restartTimer();
+            loadQueAns(init.index)
+            init.index++;
+            init.index >= 10 && finishTest();
          }
-    });
-    
-  });
 
-});
-
-
- 
- 
-
-
-function reset() {
-  var timer = document.getElementById('sec'),
-      parent = timer.parentElement;
-    if (timer) {
-      parent.removeChild(timer);
-      parent.insertAdjacentHTML('afterbegin', `<h1 id="sec">30</h1>`)
-    }
-  
+    NEXT_BTN.addEventListener('click', () =>  init.index < 10 && loadFunctionalities());
 }
 
 
-function startTimer() {
-  var timer = document.getElementById('sec');
-  let c = 29;
-  setInterval( () => {
-   if (timer.innerHTML > 0) {
-     timer.innerHTML = c;
-     c--;
-     c <= 5 && timer.classList.add('danger');
-   } 
-  }, 1000);
+function resetTimer() {
+      const NODES = {
+        timer: document.getElementById('sec'),
+        get new_h1 () {
+          var h = document.createElement('h1');
+          h.id = 'sec';
+          h.innerHTML = 30;
+          return h;
+        },
+        get nodeparent () {
+          return this.timer.parentElement;
+        }
+      }
+     
+    if (NODES.timer) 
+    NODES.nodeparent.replaceChild(NODES.new_h1,NODES.timer);  
+}
 
+
+function restartTimer() {
+  let h1 = document.getElementById('sec'),
+      c = 29;
+
+    function  timer () {
+      function decrement () {
+        h1.innerHTML = c;
+        c--;
+        c <= 5 && h1.classList.add('danger');
+      }
+      h1.innerHTML > 0 && decrement();
+    }
+
+  setInterval( () =>  timer(), 1000);
 }
 
 
 
-
-let data = [
-  {
-    question: 'When did Kenya gain independence?',
-    answers: ['1992', '1990', '1963', '1920'],
-    get corrAns () {
-      return this.answers[2];
-    }
-  },
-
-  {
-    question: 'Which one of the following is not a branch of Computer Science?',
-    answers: ['Data Collection', 'Data Science', 'Artificial Intelligence', 'Software Engineering'],
-    get corrAns () {
-      return this.answers[0];
-    }
-  },
-
-  {
-    question: 'Which one of the following is not a programming language?',
-    answers: ['Python', 'HTML', 'C++', 'C#'],
-    get corrAns () {
-      return this.answers[3];
-    }
-  },
-
-  {
-    question: '(a = 53) === (a === "53")',
-    answers: ['True', 'False', 'Undefined', 'Null'],
-    get corrAns () {
-      return this.answers[1];
-    }
-  },
-
-  {
-    question: 'Pick the odd one out?',
-    answers: ['Raila', 'Uhuru', 'Obama', 'Trump'],
-    get corrAns () {
-      return this.answers[0];
-    }
-  },
-
-  {
-    question: 'What is the capital city of Kenya?',
-    answers: ['Kisumu', 'Nairobi', 'Mombasa', 'Dodoma'],
-    get corrAns () {
-      return this.answers[1];
-    }
-  },
-
-  {
-    question: 'Pick the odd one out',
-    answers: ['Python', 'CSS', 'C++', 'C#'],
-    get corrAns () {
-      return this.answers[1];
-    }
-  },
-
-  {
-    question: 'CSS stands for?',
-    answers: ['Casscading Styles Sheets', 'Common Styles Sheets', 'Casscading Style Scripts', 'Common Styles Script'],
-    get corrAns () {
-      return this.answers[0];
-    }
-  },
-
-  {
-    question: 'Start a multiline JavaScript comment.',
-    answers: ['/*', '*/', '/* */', '//'],
-    get corrAns () {
-      return this.answers[0];
-    }
-  },
-
-  {
-    question: 'Which one of the following  statement is false about JavaScript?',
-    answers: ['Arrays are objects', 'functions are variables', 'constants cannot be reassigned', 'all of the above'],
-    get corrAns () {
-      return this.answers[3];
-    }
-  },
-
-
-  {
-    question: 'What is you favourite color?',
-    answers: ['blue', 'black', 'white', 'red'],
-    get corrAns () {
-      return this.answers[0];
-    }
-  },
-
-];
-
- 
-
-function loadQuzz() {
-    
+function loadQueAns() {
  let no = arguments[0];
 
-  let quzz_answers = {
-    quzzHolder: document.getElementById('quzz'),
+  let  que_ans = {
+    que: document.getElementById('quzz'),
     ansA : document.getElementById('a'),
     ansB : document.getElementById('b'),
     ansC : document.getElementById('c'),
     ansD : document.getElementById('d')
   };
+  
+   que_ans. que.innerHTML = 'No.' + (init.index + 1) + ' ' + data[no].question;
+   que_ans.ansA.innerHTML = data[no].answers[0];
+   que_ans.ansB.innerHTML = data[no].answers[1];
+   que_ans.ansC.innerHTML = data[no].answers[2];
+   que_ans.ansD.innerHTML = data[no].answers[3]; 
+
+  init.radios[0].value = data[no].answers[0];
+  init.radios[1].value = data[no].answers[1];
+  init.radios[2].value = data[no].answers[2];
+  init.radios[3].value = data[no].answers[3];
+
+}
 
 
-  quzz_answers.quzzHolder.innerHTML = data[no].question;
-  quzz_answers.ansA.innerHTML = data[no].answers[0];
-  quzz_answers.ansB.innerHTML = data[no].answers[1];
-  quzz_answers.ansC.innerHTML = data[no].answers[2];
-  quzz_answers.ansD.innerHTML = data[no].answers[3];
+
+/* determine score */
+init.radios.forEach(rad => {
+   rad.addEventListener('click', () => {
+     if (rad.value == data[init.index].corrAns) {
+       init.score++;
+       // remove click eventListener
+     }
+   });
+   
+});
 
 
+/* finish Test */
+function finishTest() {
+  const OUTPUTS_DIVS = {
+    ansWrapper: document.getElementById('examCard'),
+    ansHolder: document.querySelector('.answers-holder'),
+  }
+  
+  OUTPUTS_DIVS.ansWrapper.textContent = '';
+  OUTPUTS_DIVS.ansWrapper.insertAdjacentHTML('afterbegin', insertResultDiv());
+
+  document.getElementById('totQue').value = init.index;
+  document.getElementById('corrScore').value = init.score;
+
+  var per = parseInt(init.score * 100) / parseInt(init.index);
+  console.log(per)
 
 }
 
@@ -174,18 +125,4 @@ function loadQuzz() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
